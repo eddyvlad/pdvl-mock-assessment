d2312dc8cac954679267ccf3ffc4fde323828a1b
eddy <eddyvlad@gmail.com>
2025-09-14 03:47:03 +0800

    feat(ui): Add new icon, refine styles, and enhance metadata
    - Introduced new `icon.svg` for branding and removed old `favicon.ico`.
    - Updated `globals.css` with typography body color variables for better theming.
    - Expanded metadata in `layout.tsx` to include open graph details.
    - Added `HOST` to `.env.example` for better configuration.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,23 @@ import NavBar from '@/components/nav-bar';
 import { GoogleAnalytics } from '@next/third-parties/google';
 import { ReactNode } from 'react';
 
+const host = process.env.HOST ?? 'http://localhost:3000';
 export const metadata: Metadata = {
-  title: 'PDVL Mock Assessments',
-  description: 'Practice exams for the PDVL course',
+  metadataBase: new URL(host),
+  title: 'PDVL Practice Tests',
+  description: 'Practice tests for the Singapore PDVL course',
+  openGraph: {
+    title: 'PDVL Practice Tests',
+    description: 'Study smarter for Singapore\'s PDVL: module notes, quizzes, and trick question practice built around the official syllabus. Fast reviews, instant feedback',
+    locale: 'en_SG',
+    url: '/',
+    images: [{
+      url: '/og-image.png',
+      width: 1200,
+      height: 630,
+      alt: 'PDVL Practice Tests',
+    }]
+  }
 };
 
 export default function RootLayout({children}: { children: ReactNode }) {
